3e30d219b20c55ae5f9802cb93b9b009f522dde2
Vaskar Sarma <vaskar.sarma@emirates.com>
2020-05-24 19:09:42 +0400

    initial commit

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classes from "./Cockpit.css";
+import AUX from '../../hoc/Hoc-aux';
+
+const Cockpit = props => {
+  let btnclass = classes.Button;
+  if (props.showPerson) 
+    btnclass = [classes.Button, classes.red].join(' ');
+
+  const assignedClasses = [];
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold);
+  }
+
+  return (
+    <AUX>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(" ")}>Testing My application</p>
+      <button className={btnclass} onClick={props.clicked}>
+        Toggle Person
+      </button>
+      <button onClick={props.login} >Log In</button>
+    </AUX>
+  );
+};
+
+export default Cockpit;
---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import classes from "./Person.css";
+import AUX from "../../../hoc/Hoc-aux";
+import WithClasses from "../../../hoc/WithClasses";
+import PropTypes from "prop-types";
+
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[Person.JS] - inside constructor");
+    this.personTextbox= React.createRef();
+  }
+
+  componentWillMount() {
+    console.log("[Person.JS] - inside componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("[Person.JS] - inside componentDidMount");
+    if (this.props.position == 0) this.personTextbox.current.focus();
+  }
+
+  render() {
+    console.log("[Person.JS] - inside render");
+    return (
+      <AUX>
+        {this.props.isAuthnticated? 
+        <h2> I am Authenticated</h2> : null }  
+        
+        <h2 onClick={this.props.click}>
+          My name is {this.props.name} and I am {this.props.age} years old!!!
+        </h2>
+        <p>{this.props.children}</p>
+        <input ref={this.personTextbox}
+          type="text"
+          onChange={this.props.change}
+          value={this.props.name}
+        />
+        {/* <h2>My name is Vaskar Sarma and I am {Math.floor(Math.random() * 30)} years old.</h2> */}
+      </AUX>
+    );
+  }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  change: PropTypes.func
+};
+
+export default WithClasses(Person, classes.Person);
